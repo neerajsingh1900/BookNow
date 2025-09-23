f18fff7fd5a76d51fc0483b28c655c2f817d6490
neerajsingh190 <neeraj199322@gmail.com>
2025-09-23 10:17:45 +0530

    Add project files.

---
+++ BookNow/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookNow.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
